4d95eda9d5432cb00a386f58efbc39cd5e8fcaf7
ahmsay <ahmsay@gmail.com>
2022-11-06 15:53:46 +0300

    auto build works

--- client/src/App.vue
+++ client/src/App.vue
@@ -13,7 +13,7 @@
                     Your number: {{ firstGuess }}
                   </div>
                   <div class="text-white">
-                    Second guesss: {{ secondGuess }}
+                    Second guess: {{ secondGuess }}
                   </div>
                 </v-card-text>
                 <v-card-text class="pt-0 pb-2">
